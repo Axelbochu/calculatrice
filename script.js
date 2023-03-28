2642f248ea2e4dee846f399c6c9bf581a35a94cb
AxelBochu <axel.bochu@student.junia.com>
2023-03-28 10:57:28 +0200

    Step3 end

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-function clickEvent(button){
-    //on ajoute la nouvelle touche au input contenant le calcul
-    let inputCalc = document.querySelector(".inputCalc");
-    inputCalc.value = inputCalc.value + button.textContent;
+class BaseCalculator{
+    constructor(){
+        this.inputCalc = document.querySelector(".inputCalc");
+        this.result = document.querySelector(".resultH1");
+    }
+
+    clickEvent(button){
+        this.inputCalc.value = this.inputCalc.value + button.textContent;
+    }
+
+    resetCalc(){
+        //si notre calcul est encore à l'écran on l'efface
+        if(this.inputCalc.value != ""){
+            this.inputCalc.value = "";
+            this.inputCalc.style.background = "#ffffff" //on remet la couleur d'erreur par default
+        }
+        else{ //sinon on efface le resultat
+            this.result.textContent = "";
+        }
+    }
+
+    makeCalc(){
+        if(this.inputCalc.value != ""){ //si notre calcul n'est pas vide
+            try{ //on essaie de faire le calcul
+                this.result.textContent = eval(this.inputCalc.value);
+            }catch (error){ //si eval() n'as pas fonctionné, la syntaxe n'etait pas bonne et on met une couleur d'erreur
+                this.inputCalc.style.background = "#f35b5b";
+            }   
+        }
+        else{ //sinon on affiche 0
+            this.result.textContent = "0";
+        }
+    }
 }
 
-function resetCalc(){
-    let inputCalc = document.querySelector(".inputCalc");
-    //si notre calcul est encore à l'écran on l'efface
-    if(inputCalc.value != ""){
-        inputCalc.value = "";
-        inputCalc.style.background = "#ffffff" //on remet la couleur d'erreur par default
-    }
-    else{ //sinon on efface le resultat
-        document.querySelector(".resultH1").textContent = "";
-    }
-}
-
-function makeCalc(){
-    let inputCalc = document.querySelector(".inputCalc");
-    if(inputCalc.value != ""){ //si notre calcul n'est pas vide
-        try{ //on essaie de faire le calcul
-            document.querySelector(".resultH1").textContent = eval(inputCalc.value);
-        }catch (error){ //si eval() n'as pas fonctionné, la syntaxe n'etait pas bonne et on met une couleur d'erreur
-            inputCalc.style.background = "#f35b5b";
-        }   
-    }
-    else{ //sinon on affiche 0
-        document.querySelector(".resultH1").textContent = "0";
-    }
-}
+//on créer notre objet
+let Calculatrice = new BaseCalculator;
