1c25aaed08f23aebf27f0a8dfa5b8c78fd072de8
AxelBochu <axel.bochu@student.junia.com>
2023-03-28 10:41:40 +0200

    step3 start

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 function clickEvent(button){
+    //on ajoute la nouvelle touche au input contenant le calcul
     let inputCalc = document.querySelector(".inputCalc");
     inputCalc.value = inputCalc.value + button.textContent;
 }
 
 function resetCalc(){
     let inputCalc = document.querySelector(".inputCalc");
+    //si notre calcul est encore à l'écran on l'efface
     if(inputCalc.value != ""){
         inputCalc.value = "";
-        inputCalc.style.background = "#ffffff"
+        inputCalc.style.background = "#ffffff" //on remet la couleur d'erreur par default
     }
-    else{
+    else{ //sinon on efface le resultat
         document.querySelector(".resultH1").textContent = "";
     }
 }
 
 function makeCalc(){
     let inputCalc = document.querySelector(".inputCalc");
-    if(inputCalc.value != ""){
-        try{
+    if(inputCalc.value != ""){ //si notre calcul n'est pas vide
+        try{ //on essaie de faire le calcul
             document.querySelector(".resultH1").textContent = eval(inputCalc.value);
-        }catch (error){
+        }catch (error){ //si eval() n'as pas fonctionné, la syntaxe n'etait pas bonne et on met une couleur d'erreur
             inputCalc.style.background = "#f35b5b";
         }   
+    }
+    else{ //sinon on affiche 0
+        document.querySelector(".resultH1").textContent = "0";
     }
 }
